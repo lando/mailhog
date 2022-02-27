09046cbaaf294f7f1f956ff2177b74ccc5998d5b
Mike Pirog <mike@lando.dev>
2022-02-26 20:07:35 -0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,13 +26,13 @@ module.exports = {
     sidebar: [
       {
         text: 'Getting Started',
-        link: '/index.md',
+        link: '/index.html',
       },
-      '/config.md',
-      '/support.md',
+      '/config.html',
+      '/support.html',
       {text: 'Examples', link: 'https://github.com/lando/mailhog/tree/main/examples'},
       {text: 'Release Notes', link: 'https://github.com/lando/mailhog/releases'},
-      '/development.md',
+      '/development.html',
     ],
   },
 };
